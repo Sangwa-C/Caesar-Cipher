658221f3a2c914269666d43d247e5f06a04e13a2
Rithaven <veneruth4@gmail.com>
2019-09-07 01:16:10 +0200

    adding functions

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -6,8 +6,17 @@ public class CaesarCipher {
     public static void main(String args[]) {
         Scanner cece= new Scanner(System.in);
         System.out.println(" Write your message here ....");
+        String ciphertext = cece.nextLine();
         int koti;
-        char toti;
+        char text;
+        System.out.println("Write a shift value...... ");
+        koti = cece.nextInt();
+        ciphertext = cece.nextLine();
 
+        for(int i=0; i < ciphertext.length();i++){
+            if(text >= 'a' && text <= 'z'){
+                text = (char)(text + koti);
+            }
+        }
     }
 }
